e8c03acfcab1fe7cb735423d5d0afb9c4d92ad37
EvgeniyKozenok <kozenokevgeniy@gmail.com>
2020-11-13 00:05:37 +0200

    add search api controller and handle ajax request

---
+++ app/Http/Controllers/Api/SearchController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Houses;
+use Illuminate\Http\JsonResponse as JsonResponse;
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function find(Request $request)
+    {
+        $q = Houses::where('id', '>', 0);
+
+        $request->whenFilled('name', function ($input) use (&$q) {
+            $q->whereRaw("LOWER(`name`) LIKE '%" . strtolower($input) . "%'");
+        });
+
+        foreach (['bedrooms', 'bathrooms', 'storeys',  'garages'] as $exactMatchKey) {
+            $request->whenFilled($exactMatchKey, function ($input) use (&$q, $exactMatchKey) {
+                $q->where($exactMatchKey, $input);
+            });
+        }
+
+        $request->whenFilled('price', function ($input) use (&$q) {
+            $q->whereBetween('price', [$input[0], $input[1]]);
+        });
+
+        $response = [
+            'success' => true,
+            'data'    => $q->get(),
+            'message' => 'lalalal',
+        ];
+
+        return response()->json($response);
+    }
+}
