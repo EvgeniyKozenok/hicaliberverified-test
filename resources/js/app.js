682dc291ecfd59c0fb3782647cadda2672d9a4a2
EvgeniyKozenok <kozenokevgeniy@gmail.com>
2020-11-13 00:24:43 +0200

    vue frontend start

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
+import Vue from 'vue';
+import DeveloperTest from './components/DeveloperTest'
+
 require('./bootstrap');
+
+Vue.config.productionTip = false;
+Vue.config.runtimeCompiler = true;
+
+new Vue({
+    el: '#app',
+    template: '<DeveloperTest/>',
+    components: { DeveloperTest }
+});
---
+++ resources/js/components/DeveloperTest.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>{{ hello }}</div>
+</template>
+
+<script>
+export default {
+    name: "DeveloperTest",
+    data() {
+        return {
+            hello: 'Hello from Vue',
+        }
+    },
+    beforeMount() {
+        console.log(this.hello);
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
